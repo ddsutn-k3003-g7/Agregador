2f39a0d8a5d779f63b598c4d6f3bd5cbb72a702e
LuichiTW <ltitaewatanabe@frba.utn.edu.ar>
2025-09-12 02:20:06 -0300

    mejoras consensoController

--- src/main/java/ar/edu/utn/dds/k3003/controller/ConsensoController.java
+++ src/main/java/ar/edu/utn/dds/k3003/controller/ConsensoController.java
@@ -2,6 +2,8 @@ package ar.edu.utn.dds.k3003.controller;
 
 import ar.edu.utn.dds.k3003.facades.FachadaAgregador;
 import ar.edu.utn.dds.k3003.facades.dtos.ConsensosEnum;
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -9,6 +11,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@Slf4j
 @RestController
 @RequestMapping("/api/consenso")
 public class ConsensoController {
@@ -23,7 +28,10 @@ public class ConsensoController {
     @PatchMapping
     public ResponseEntity<Void> actualizarEstrategiaConsenso(
             @RequestBody ConsensoRequest request) {
-        
+
+         log.info("Request recibido: tipoConsenso={}, coleccionId={}", 
+             request.getTipoConsenso(), request.getColeccionId());
+
         fachadaAgregador.setConsensoStrategy(
             request.getTipoConsenso(), 
             request.getColeccionId()
@@ -34,8 +42,14 @@ public class ConsensoController {
 
     // Clase interna para el request body
     public static class ConsensoRequest {
+
+        @JsonProperty("tipoConsenso")
         private ConsensosEnum tipoConsenso;
+
+        @JsonProperty("coleccionId")
         private String coleccionId;
+
+        public ConsensoRequest() {}
 
         // Getters y setters
         public ConsensosEnum getTipoConsenso() {
