237f9235a4b16c9ce159fb38de0aa23b1e185cd8
LuichiTW <ltitaewatanabe@frba.utn.edu.ar>
2025-11-14 23:51:02 -0300

    fix mal manejo arrays

--- src/main/java/ar/edu/utn/dds/k3003/controller/ColeccionController.java
+++ src/main/java/ar/edu/utn/dds/k3003/controller/ColeccionController.java
@@ -50,7 +50,7 @@ public class ColeccionController {
         if (hechos.size() > (page * size) + size) {
             toIndex = Math.min((page * size) + (size - 1), hechos.size());
         }else if (hechos.size() <= (page * size) + size) {
-            toIndex = (page * size) + size;
+            toIndex = hechos.size() - 1;
         }
 
         hechosPagina= hechos.subList(fromIndex, toIndex);
